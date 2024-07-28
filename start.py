367e0850383f55cd658cf0bd0339294150fba2c7
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2024-07-28 11:37:40 +0200

    Automatic concert update

--- start.py
+++ start.py
@@ -25,29 +25,40 @@ def choose_concert(concerts):
     answers = prompt(questions)
     return answers
 
+
 def update_file(concert_file):
+    update_line = False
+
     with open(f"pages/{YEAR}/{concert_file}", "r") as file:
         content = file.read()
-        
-        print(content)
-    
-    # with open(f"pages/{YEAR}/{concert_file}", "w") as file:
-    #     file.write(content)
+
+        for line in content.split("\n"):
+            if "# Update" in line:
+                update_line = True
+                continue
+
+            if update_line:
+                content = content.replace(line, f"src: ./pages/{YEAR}/{concert_file}")
+
+    with open(f"pages/{YEAR}/{concert_file}", "w") as file:
+        file.write(content)
 
 
 def main():
-    
+
     print("Updating the repository...")
     update_process = os.system("git pull")
     if update_process != 0:
         print("Error updating the repository.")
-        if prompt([
-            {
-                "type": "confirm",
-                "name": "reset",
-                "message": "Do you want to reset the repository and continue?",
-            }
-        ])["reset"]:
+        if prompt(
+            [
+                {
+                    "type": "confirm",
+                    "name": "reset",
+                    "message": "Do you want to reset the repository and continue?",
+                }
+            ]
+        )["reset"]:
             reset_process = os.system("git reset --hard origin/main")
             update_again_process = os.system("git pull")
             if reset_process != 0 or update_again_process != 0:
@@ -56,34 +67,36 @@ def main():
         else:
             print("Exiting...")
             return
-        
+
     print("\nRepository updated.\n\n")
-    
+
     concert = choose_concert(get_concerts())
     concert = concert.get("concert")
 
     if not concert:
         print("No concert selected. Exiting...")
         return
-    
+
     print(f"Updating concert {concert}...")
     update_file(concert)
     print("Concert updated.")
-    
+
     print("Pushing changes to the repository...")
     push_process = os.system("git add .")
     push_process += os.system("git commit -m 'Automatic concert update'")
-    
+    push_process += os.system("git push")
+
     if push_process != 0:
         print("Error pushing changes to the repository.")
         return
 
     print("Changes pushed to the repository.")
-    
+
     print("Starting the server...")
     print("Press Ctrl+C to stop the server.")
-    
+
     os.system("npm run dev")
+
 
 if __name__ == "__main__":
     main()
